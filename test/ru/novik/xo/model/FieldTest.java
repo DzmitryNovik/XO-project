aafd790fb2174d9517db17fccacd9bc30ddd8e17
Dzmitry Novik <nowikmitya@yandex.ru>
2022-03-18 18:10:03 +0300

    FieldTest with Field (with flexible size)

--- test/ru/novik/xo/model/FieldTest.java
+++ test/ru/novik/xo/model/FieldTest.java
@@ -12,13 +12,13 @@ class FieldTest {
 
     @Test
     void getSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         assertEquals(3, field.getSize());
     }
 
     @Test
     void setFigure() throws InvalidPointException, AlreadyOccupiedException {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
 
@@ -30,7 +30,7 @@ class FieldTest {
 
     @Test
     void getFigureWhenFigureIsNonSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -40,7 +40,7 @@ class FieldTest {
 
     @Test
     void getFigureWhenXIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1, 0);
 
         try {
@@ -51,7 +51,7 @@ class FieldTest {
 
     @Test
     void getFigureWhenYIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -2);
 
         try {
@@ -62,7 +62,7 @@ class FieldTest {
 
     @Test
     void getFigureWhenXIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
 
         try {
@@ -73,7 +73,7 @@ class FieldTest {
 
     @Test
     void getFigureWhenYIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
 
         try {
