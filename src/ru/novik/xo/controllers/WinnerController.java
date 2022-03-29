893a8ff0567518c52cd3018cabf08af94a6d6140
Dzmitry Novik <nowikmitya@yandex.ru>
2022-03-29 19:24:02 +0300

    fixes for WinnerController

--- src/ru/novik/xo/controllers/WinnerController.java
+++ src/ru/novik/xo/controllers/WinnerController.java
@@ -7,6 +7,7 @@ import ru.novik.xo.model.exceptions.InvalidPointException;
 import java.awt.*;
 
 public class WinnerController {
+
     public Figure getWinner(final Field field) {
         try {
             for (int i = 0; i < 3; i++) {
@@ -20,31 +21,34 @@ public class WinnerController {
                 }
             }
             if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1))) {
-                return field.getFigure(new Point(1, 1));
+                return field.getFigure(new Point(0, 0));
             }
             if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1))) {
                 return field.getFigure(new Point(1, 1));
             }
 
-        } catch (InvalidPointException e) {
+        } catch (final InvalidPointException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator) {
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) {
         final Figure currentFigure;
         final Figure nextFigure;
         final Point nextPoint = pointGenerator.next(currentPoint);
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+
+            if (currentFigure == null) {
+                return false;
+            }
+
+            nextFigure = field.getFigure(nextPoint);
         } catch (final InvalidPointException e) {
             return true;
-        }
-
-        if (currentFigure == null) {
-            return false;
         }
 
         if (currentFigure != nextFigure) {
